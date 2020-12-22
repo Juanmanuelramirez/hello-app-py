cd983e2975c118993ebc05ceb3f189f37ebf7472
Juan Ramirez <juanmanuelramirez@users.noreply.github.com>
2020-12-22 12:42:36 -0600

    se corrigue error

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # [START hello-app]
 from flask import Flask
 app = Flask('hello-cloudbuild')
@@ -6,8 +20,6 @@ app = Flask('hello-cloudbuild')
 def hello():
   return "Hello World!\n"
 
-# Usar solo cuando se corre localmente. cuando se despliegue en Cloud Run,
-# un servidor de preosesos como Gunicorn sirve para el app.
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
 # [END hello-app]
